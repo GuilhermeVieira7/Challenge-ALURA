0cb191450cf40aadc41bede214bbb250c36edc6d
GuilhermeVieira7 <guilhermevieira3356@gmail.com>
2025-08-05 22:54:11 -0300

    Implementa destaque no botão Adicionar quando campo preenchido

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []
+
+function adicionarAmigo() {
+    let amigo = document.getElementById('amigo').value;
+    if (amigo == '') {
+        alert('Por favor, insira um nome.');
+    } else {
+        amigos.push(amigo);
+        document.getElementById('amigo').value = '';    
+        atualizarLista();
+        verificarCampo();
+    }
+}
+
+function verificarCampo() {
+    let campo = document.getElementById('amigo').value;
+    let botao = document.getElementById('Adicionar');
+    if (campo.trim() !== "") {
+        botao.style.backgroundColor = "green";
+        botao.style.color = "white";
+    } else {
+        botao.style.backgroundColor = "";
+        botao.style.color = "";
+    }
+}
+   function atualizarLista() {
+        document.getElementById('listaAmigos').innerHTML = amigos.map(nome => `<li>${nome}</li>`).join('');
+    }
+
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert('Nenhum amigo para sortear. Por favor, adicione amigos primeiro.');
+    } else {
+        let indiceSorteado = Math.floor(Math.random() * amigos.length);
+        let amigoSorteado = amigos[indiceSorteado];
+        document.getElementById('resultado').innerHTML = `O amigo sorteado é: <strong>${amigoSorteado}</strong>`;
+        amigos.splice(indiceSorteado, 1);
+    }
+}
